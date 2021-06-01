20adc74cde45d7906c50d696ab1ebc2ea216955f
awenzelhuemer <43669899+Superandi1234@users.noreply.github.com>
2021-06-01 11:38:50 +0200

    Add 2048 game

---
+++ src/main/java/spw4/game2048/Game.java
@@ -0,0 +1,61 @@
+package spw4.game2048;
+
+import java.util.Random;
+
+public class Game {
+
+    public Board board;
+    private int moves;
+
+    public Game() {
+        board = new Board();
+        Board.random = new Random(1234);
+    }
+
+    public int getScore() {
+        return board.getPoints();
+    }
+
+    public int getMoves() {
+        return moves;
+    }
+
+    public boolean isOver() {
+        return !board.hasValidMoves() || isWon();
+    }
+
+    public boolean isWon() {
+        return board.contains2048();
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("Moves: ");
+        builder.append(getMoves());
+
+        builder.append("\tScore: ");
+        builder.append(getScore());
+
+        builder.append("\n");
+        builder.append(board);
+
+        return builder.toString();
+    }
+
+    public void initialize() {
+        board.initialize();
+    }
+
+    public void move(Direction direction) {
+        if(board.move(direction)) {
+            moves++;
+        }
+    }
+
+    public String getValueAt(int row, int column) {
+        return String.valueOf(board.getTileValue(row, column));
+    }
+}
